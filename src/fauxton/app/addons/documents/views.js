b7e7775ff43e16ed68f8a54f277e6354d787d47b
suelockwood <deathbear@apache.org>
2013-12-17 12:11:30 -0500

    We use the AdvancedOptions View in a couple spots where the eventer isn't passed. Added logic so it will only bind the listener if it is

--- src/fauxton/app/addons/documents/views.js
+++ src/fauxton/app/addons/documents/views.js
@@ -1110,7 +1110,9 @@ function(app, FauxtonAPI, Components, Documents, Databases, pouchdb, resizeColum
         this.showPreview = options.showPreview;
       }
 
-      this.listenTo(this.eventer, 'options:param_update', this.optionsParamsUpdate);
+      if (this.eventer) { 
+        this.listenTo(this.eventer, 'options:param_update', this.optionsParamsUpdate);
+      }
     },
 
     events: {
